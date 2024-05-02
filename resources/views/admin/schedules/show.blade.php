1f84407cd328cc75ceab61fa42c8bca7975e273a
biobios <teiketsuatsu.lbp@gmail.com>
2024-05-02 23:50:29 +0900

    バグ修正

--- resources/views/admin/schedules/show.blade.php
+++ resources/views/admin/schedules/show.blade.php
@@ -7,12 +7,12 @@
     <title>スケジュール</title>
 </head>
 <body>
-    <h1>{{$schedule->$movie->title}}</h1>
+    <h1>{{$schedule->movie->title}}</h1>
     <div>
-        <img src="{{$schedule->$movie->image_url}}" alt="{{$schedule->$movie->title}}">
+        <img src="{{$schedule->movie->image_url}}" alt="{{$schedule->movie->title}}">
     </div>
-    <p>公開年：{{$schedule->$movie->published_year}}</p>
-    <p>{{$schedule->$movie->description}}</p>
+    <p>公開年：{{$schedule->movie->published_year}}</p>
+    <p>{{$schedule->movie->description}}</p>
     <p>開始時間：{{$schedule->start_time}}</p>
     <p>終了時間：{{$schedule->end_time}}</p>
     <a href="/admin/schedules/{{ $schedule->id }}/edit">編集</a>
